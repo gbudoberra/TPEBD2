78b551989b8edffd14a56e51a9200ebf0c62c69e
bsquillari <bsquillari@itba.edu.ar>
2022-12-06 01:43:20 -0300

    PostgreSQL configuration + Test request

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+/**
+ * - POST /user                         PostgreSQL
+ * - POST /login                        PostgreSQL
+ *
+ * - POST /files                        MongoDB + PostgreSQL
+ * - GET /file/{docId}                  MongoDB
+ * - DELETE /file/{docId}               MongoDB + PostgreSQL
+ * - PUT /file/{docId}                  MongoDB
+ * - GET /files/{userId}                PostgreSQL
+ *
+ * - POST /file/{docId}/editors         PostgreSQL
+ * - DELETE /file/{docId}/editors       PostgreSQL
+ * - GET /file/{docId}/editors          PostgreSQL
+ * - GET /files/shared/{userId}         PostgreSQL
+ *
+ */
+
+
 const express = require("express");
 const morgan = require("morgan"); //Logging
 const mongoose = require("mongoose");
@@ -6,8 +24,19 @@ const notesSchema = require("./src/models/notes");
 const { updateOne } = require("./src/models/notes");
 require("dotenv").config();
 
+const pgp = require("pg-promise")(/*options*/);
+const postgreSQL = pgp("postgres://sfdimfjr:6lpzaG4vr29YeiGmntl516Rm48W1Dmze@babar.db.elephantsql.com/sfdimfjr");
+
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.get("/test", (req, res) => {
+    postgreSQL.query("SELECT * from test").then(
+        (queryResult) => {
+            res.send(queryResult);
+        }
+    );
+})
 
 app.get("/files", (req, res) => {
   notesSchema
@@ -68,14 +97,22 @@ app.get("/files/:id", (req, res) => {
     });
 });
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+postgreSQL.connect()                                            // Connect to postgreSQL
+    .then(() => {
+        console.log("PostgreSQL: DB connected");
+        mongoose.connect(process.env.MONGODB_URI)               // Then, connect to MongoDB
+            .then(() => {
+                console.log("MongoDB: DB connected");
+                app.listen(3000);                          // Finally, run server
+                                                                // TODO: Agregar parametros al server o dejar los default?
+                console.log("Server on port 3000");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-app.listen(3000);
-console.log("Server on port 3000");
+
